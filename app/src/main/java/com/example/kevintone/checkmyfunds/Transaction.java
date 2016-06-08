11831339a6c85dc23f69cf293b7d6a2852beeaf4
kevintone <kevinton@hawaii.edu>
2016-06-07 23:55:22 -1000

    Added Transaction class to hold multiple values + gets/sets

---
+++ app/src/main/java/com/example/kevintone/checkmyfunds/Transaction.java
@@ -0,0 +1,50 @@
+package com.example.kevintone.checkmyfunds;
+
+/**
+ * Created by kevintone on 6/7/16.
+ */
+public class Transaction {
+    private String description;
+    private String dateTime;
+    private String classText;
+    private Double amountToChange;
+
+    public Transaction(String description, String dateTime, String classText, Double amountToChange) {
+        this.description = description;
+        this.dateTime = dateTime;
+        this.classText = classText;
+        this.amountToChange = amountToChange;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDateTime() {
+        return this.dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getClassText() {
+        return this.classText;
+    }
+
+    public void setClassText(String classText) {
+        this.classText = classText;
+    }
+
+    public Double getAmountToChange() {
+        return this.amountToChange;
+    }
+
+    public void setAmountToChange(Double amountToChange) {
+        this.amountToChange = amountToChange;
+    }
+}
